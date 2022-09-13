b7e89dc1b13ceff9fed43b1ded2828e82a991b15
Kaan ÖZER <kaanozer35@gmail.com>
2022-09-13 19:06:49 +0300

    Test stage

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,13 +4,20 @@
 const countdownEl = document.querySelector(".countdown");
 const btnStartStopEl = document.querySelector(".btn-start-stop");
 
-const startingMinutes = 20;
+/* variables */
 
+//pomodoro counter
+let pomodoroCounter = 0;
+//poomodoro starting minutes
+const startingMinutes = 1;
+//short break starting minutes
+const shortBreakMinutes = 5;
 // time from second type
 let totalTime = startingMinutes * 60;
+let refreshInterval;
 
 /* Functions */
-const updateCountdown = function () {
+function updateCountdown() {
   let minutes = Math.floor(totalTime / 60);
 
   minutes = minutes < 10 ? "0" + minutes : minutes;
@@ -26,21 +33,33 @@ const updateCountdown = function () {
   totalTime--;
 
   if (totalTime < 0) {
-    //stop the setInterval when time = 0 for avoid negative time
-    clearInterval(refreshInterval);
+    pomodoroCounter++;
+
+    if (pomodoroCounter === 4) {
+      document.getElementsById("btn-pomodoro").classList.remove("active-btn");
+    } else {
+      document.getElementsById("btn-shortBreak").classList.add("active-btn");
+      totalTime = shortBreakMinutes * 60;
+      countdownEl.innerHTML = `${minutes}:${seconds}`;
+      //stop the setInterval when time = 0 for avoid negative time
+      clearInterval(refreshInterval);
+    }
   }
-};
+}
 
 // let refreshInterval = setInterval(updateCountdown, 1000);
-let refreshInterval;
+
 btnStartStopEl.addEventListener("click", function () {
   console.log(btnStartStopEl.textContent);
+
   if (btnStartStopEl.textContent === "START") {
     refreshInterval = setInterval(updateCountdown, 1000);
+
     btnStartStopEl.innerHTML = "STOP";
     btnStartStopEl.style.backgroundColor = " rgba(52, 58, 64, 0.447)";
   } else if (btnStartStopEl.textContent === "STOP") {
     clearInterval(refreshInterval);
+
     btnStartStopEl.innerHTML = "START";
     btnStartStopEl.style.backgroundColor = " rgba(222, 226, 230, 0.193)";
   }
